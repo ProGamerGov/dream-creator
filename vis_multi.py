e50dae6c368afa86f94bdf34226adbc21df5a1ef
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-09-23 15:04:12 -0600

    Update vis_multi.py

--- vis_multi.py
+++ vis_multi.py
@@ -80,7 +80,7 @@ def main_func(params):
     # Preprocessing net layers
     mod_list = []
     if params.fft_decorrelation:
-        if params.color_decorrelation:
+        if params.color_decorrelation == 'none':
             try:
                 params.color_decorrelation = torch.load(params.model_file)['color_correlation_svd_sqrt']
             except:
