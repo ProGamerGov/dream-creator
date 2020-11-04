924573fd18be9c14d037797b3083adb0b7a07dd0
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-11-04 09:10:49 -0700

    Update vis.py

--- vis.py
+++ vis.py
@@ -61,7 +61,7 @@ def main_func(params):
     if params.content_image != '':
         params.image_size = calc_image_size(params.content_image, params.image_size)
     else:
-        assert len(image_size) > 1, "two -image_size values are required when not using a content image"
+        assert len(params.image_size) > 1, "two -image_size values are required when not using a content image"
 
     if params.seed > -1:
         set_seed(params.seed)
